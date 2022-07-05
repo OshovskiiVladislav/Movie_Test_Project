42965ee79cf3869b7f4ed770c6253b3fad9d7432
Qwerty <rosna-m@mail.ru>
2022-07-05 21:53:28 +0800

    Commit 8. add utils

---
+++ src/main/java/com/oshovskii/movie/utils/UserUtils.java
@@ -0,0 +1,18 @@
+package com.oshovskii.movie.utils;
+
+import com.oshovskii.movie.model.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserUtils {
+    private UserUtils(){
+        // No-op
+    }
+
+    public static Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+}
